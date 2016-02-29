14b649685827ae1f198ef895818124cb314c3f96
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2016-02-29 15:07:51 +0100

    Removed useless test

--- tests/unit/services/fingerprint-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit'
-
-moduleFor('service:fingerprint', 'Unit | Service | fingerprint ', {
-})
-
-test('it exists', function(assert) {
-  let service = this.subject()
-  assert.ok(service)
-})
