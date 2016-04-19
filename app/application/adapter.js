f2a407b90d90026738ee6df4bc90830bfcb68b06
Jonas Metzener <jonasmetzener@gmail.com>
2016-04-19 20:52:27 +0200

    Added basic thread validation

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,4 +1,5 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api'
+import DS             from 'ember-data'
 
 /**
  * The default application adapter
@@ -15,5 +16,26 @@ export default JSONAPIAdapter.extend({
    * @default 'api/v1'
    * @public
    */
-  namespace: 'api/v1'
+  namespace: 'api/v1',
+
+  /**
+   * Treat HTTP 400 as a validation error
+   * code response, since django-rest-framework
+   * returns HTTP 400 on validation errors
+   *
+   * @method handleResponse
+   * @param {Number} status The HTTP status code
+   * @param {Object} headers The response headers
+   * @param {Object} payload The payload of the response
+   * @return {Object|DS.InvalidError} Response
+   * @todo Import InvalidError directly if possible
+   * @public
+   */
+  handleResponse(status, headers, payload) {
+    if (status === 400 && payload.errors) {
+      return new DS.InvalidError(payload.errors)
+    }
+
+    return this._super(...arguments)
+  }
 })
